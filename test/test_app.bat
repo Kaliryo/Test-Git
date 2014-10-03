5ce247281343ebc4412ba03c3d9a0087339ebacc
Ewen ROBIN <erobin35@gmail.com>
2014-10-03 14:07:28 +0200

    update major version

---
+++ test/test_app.bat
@@ -0,0 +1,13 @@
+### test_app.bat - 201x - Ewen ROBIN ###
+
+version = "2.0.0"
+build_hour = "14h00"
+
+echo "Build${build_hour}"
+echo"version${version}"
+
+
+#echo "Copyright company - 201x"
+#echo "Build : 16:00"
+#echo "version 2.0.0"
+
