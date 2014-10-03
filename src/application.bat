3e463e44c022bacdbf11d64aa8208390afe6839a
Ewen ROBIN <erobin35@gmail.com>
2014-10-03 12:14:58 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,12 @@
 ### application.bat - 201x - Ewen ROBIN ###
-echo "Copyright company - 201x"
-echo "Build : 16:00"
-echo "version 1.1"
+
+version = "2"
+build_hour = "12h00"
+
+echo "Build${build_hour}"
+echo"version${version}"
+
+
+#echo "Copyright company - 201x"
+#echo "Build : 16:00"
+#echo "version 1.1"
