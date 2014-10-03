7866fa922cf93ccd9ab4bc8aa749a3bac75e34a1
Ewen ROBIN <erobin35@gmail.com>
2014-10-03 15:50:18 +0200

    update minor version

---
+++ test/test_app.bat
@@ -0,0 +1,13 @@
+### test_app.bat - 201x - Ewen ROBIN ###
+
+version = "1.2.1"
+build_hour = "14h00"
+
+echo "Build${build_hour}"
+echo"version${version}"
+
+
+#echo "Copyright company - 201x"
+#echo "Build : 16:00"
+#echo "version 1.2.1"
+
